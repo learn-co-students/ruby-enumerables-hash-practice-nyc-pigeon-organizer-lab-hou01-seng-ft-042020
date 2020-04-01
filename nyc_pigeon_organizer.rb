1aa7ef4d76e110dbc600777a0f4e426eed289778
YUNJUNG KIM <kyjekffo1105@yahoo.com>
2020-04-01 21:50:05 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,32 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+# write your code here!
+
+  new_hash = {}
+ 
+  data.collect do |key, value|
+    
+    value.collect do |new_key, new_value|
+
+      new_value.collect do |element|
+        
+        if !new_hash[element]
+          new_hash[element] = {}
+        end
+
+        if !new_hash[element][key]
+          new_hash[element][key] = []
+          
+        end
+        new_hash[element][key] << new_key.to_s
+
+    # "Theo" => {:color => []}
+    # << new_key.to_s
+    # binding.pry
+    
+      end
+
+    end
+    
+  end
+  new_hash
 end
