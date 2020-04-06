8c21c507daeeb3466998a97450d5da1c7da8fed8
Richard Chea <rchea817@gmail.com>
2020-04-06 02:29:17 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,23 +1,15 @@
 require "pry"
 def nyc_pigeon_organizer(data)
   # write your code here!
-  new_hash = {}
-  data.each do |property, hash|
-    hash.each do |attribute, array|
+  pigeon_list = {}
+  data.each do |category, attribute|
+    attribute.each do |attribute_value, array|
       array.each do |name|
-        if !new_hash.has_key?(name)
-          new_hash[name] = {}
-        end
-
-        if !new_hash[name].has_key?(property)
-          new_hash[name][property] = []
-        end
-
-        if !new_hash[name][property].include?(attribute)
-          new_hash[name][property] << attribute.to_s
-        end
+        pigeon_list[name] ||= {}
+        pigeon_list[name][category] ||= []
+        pigeon_list[name][category].push(attribute_value.to_s)
       end
     end
   end
-  new_hash
+  pigeon_list
 end
