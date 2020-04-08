0318954d7382fc2511d60feb11fc4e3edafc4779
Mark De La Rosa <mark.p.dlr@gmail.com>
2020-04-08 16:32:04 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+
+  data.each do |attribute_type, attributes|
+    attributes.each do |attribute_value, pigeon_names|
+      pigeon_names.each do |name|
+        pigeon_list[name] ||= {}
+        pigeon_list[name][attribute_type] ||= []
+        pigeon_list[name][attribute_type].push(attribute_value.to_s)
+      end
+    end 
+  end
+ p pigeon_list
 end
