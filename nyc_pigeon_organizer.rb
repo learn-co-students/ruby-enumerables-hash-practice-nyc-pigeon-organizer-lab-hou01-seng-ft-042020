c95baa490f599a16869a485d01e4047e2b23ea4d
Freddy Flores <floresfreddy.ultra@gmail.com>
2020-04-10 08:48:34 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
+
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  new_pigeon = {}
+ 
+  data.each do |attribute_type, attribute|
+    attribute.each do |attribute_value, pigeon_name|
+      pigeon_name.each do |name|
+        new_pigeon[name] ||= {}
+        new_pigeon[name][attribute_type] ||= []
+        new_pigeon[name][attribute_type].push(attribute_value.to_s)
+      end
+    end
+  end
+new_pigeon
 end
