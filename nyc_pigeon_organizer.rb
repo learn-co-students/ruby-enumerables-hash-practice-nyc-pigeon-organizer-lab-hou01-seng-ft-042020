ebaa793846325c67ea62023331c78d0942b3d064
edwin calvillo <calvillo.edwin@gmail.com>
2020-04-15 19:09:11 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_data_set = {}
+  data.each do |key, value|
+    value.each do |new_value, names|
+      names.each do |name|
+
+        if !new_data_set[name]
+          new_data_set[name] = {}
+        end
+
+        if !new_data_set[name][key]
+          new_data_set[name][key] =[]
+        end
+
+        new_data_set[name][key] << new_value.to_s
+      end
+    end
+  end
+  return new_data_set
 end
